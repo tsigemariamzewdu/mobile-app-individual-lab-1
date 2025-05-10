c5747c68fd8b78f42002d320dec7a400eb79a5c8
Tsigemariam <tsigemariamzewdu20@gmail.com>
2025-05-10 22:23:35 +0300

    added course data model

---
+++ app/src/main/java/com/example/mobileappplab1/Course.kt
@@ -0,0 +1,10 @@
+package com.example.mobileappplab1
+
+data class Course(
+    val id: Int,
+    val title: String,
+    val code: String,
+    val creditHours: Int,
+    val description: String,
+    val prerequisites: String
+)
