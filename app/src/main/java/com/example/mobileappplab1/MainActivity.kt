5c0e20e8c08bdff1023898680e9a1b69d8cfa439
Tsigemariam <tsigemariamzewdu20@gmail.com>
2025-05-11 06:26:48 +0300

    edited main activity to display the courselist screen

--- app/src/main/java/com/example/mobileappplab1/MainActivity.kt
+++ app/src/main/java/com/example/mobileappplab1/MainActivity.kt
@@ -3,45 +3,26 @@ package com.example.mobileappplab1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileappplab1.CourseListScreen
+import com.example.mobileappplab1.data.CourseData
 import com.example.mobileappplab1.ui.theme.MobileApppLab1Theme
 
-class MainActivity : ComponentActivity() {
+ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MobileApppLab1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CourseListScreen(courses = CourseData.sampleCourses)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileApppLab1Theme {
-        Greeting("Android")
     }
 }
